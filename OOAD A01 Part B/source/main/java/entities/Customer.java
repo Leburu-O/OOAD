3e745dc3a9cbb99870d33558e4c39f72b60c3d4e
Leburu-O <cse24-142@thuto.bac.ac.bw>
2025-10-31 22:01:18 +0000

    Update

--- OOAD A01 Part B/source/main/java/entities/Customer.java
+++ OOAD A01 Part B/source/main/java/entities/Customer.java
@@ -1,13 +1,18 @@
+// Customer.java
 package entities;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a bank customer.
+ * Holds personal details and manages linked accounts.
+ */
 public class Customer {
     private String firstName;
     private String surname;
     private String address;
-    private String accountNumber; // used as username
+    private String accountNumber; // Used as username
     private String pin;
     private List<Account> accounts;
 
@@ -20,31 +25,61 @@ public class Customer {
         this.accounts = new ArrayList<>();
     }
 
+    /**
+     * Authenticates the customer using account number and PIN.
+     * @param accNum Account number provided during login
+     * @param pin PIN provided during login
+     * @return true if credentials match, false otherwise
+     */
     public boolean authenticate(String accNum, String pin) {
         return this.accountNumber.equals(accNum) && this.pin.equals(pin);
     }
 
+    /**
+     * Adds an account to the customer's list of owned accounts.
+     * @param account The account to add
+     */
     public void addAccount(Account account) {
         accounts.add(account);
     }
 
+    /**
+     * Gets all accounts owned by this customer.
+     * @return List of accounts
+     */
     public List<Account> getAccounts() {
         return accounts;
     }
 
-    // Getters
-    public String getFirstName() { return firstName; }
-    public String getSurname() { return surname; }
-    public String getAddress() { return address; }
-    public String getAccountNumber() { return accountNumber; }
+    // ----- Getters -----
 
-    @Override
-    public String toString() {
-        return String.format("Customer{%s %s | Addr: %s}", firstName, surname, address);
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
     }
 
     public String getPIN() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPIN'");
+        return pin;
+    }
+
+    // ----- Optional: Setters (if needed for persistence) -----
+    // Not required unless loading from database
+
+    // ----- toString for debugging -----
+    @Override
+    public String toString() {
+        return String.format("Customer{%s %s | Acc: %s | Addr: %s}", 
+                firstName, surname, accountNumber, address);
     }
 }
